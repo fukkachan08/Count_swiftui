b5914d4d2bc145f9bc191e61cc3f3df713047afc
Fukano Masato <marbo0805@gmail.com>
2025-06-26 18:41:48 +0900

    完成

--- Count_swiftui/ContentView.swift
+++ Count_swiftui/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var count = 0
+    @State var result = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(result)")
+                .frame(width: 300, height: 100)
+                .font(.system(size: 32))
+            Text("\(count)")
+                .frame(width: 300, height: 100)
+                .font(.system(size: 40))
+                .foregroundColor(count >= 0 ? .red : .blue)
+            HStack{
+                Button("+"){
+                    count = count + 1
+                    if count >= 5 {
+                        result = "プラスの勝ち"
+                    } else if count <= -5 {
+                        result = "マイナスの勝ち"
+                    } else {
+                        result = ""
+                    }
+                }
+                .font(.system(size: 40))
+                .foregroundStyle(.white)
+                .frame(width: 100, height: 100)
+                .background(.red)
+                
+                Button("-"){
+                    count = count - 1
+                    if count >= 5 {
+                        result = "プラスの勝ち"
+                    } else if count <= -5 {
+                        result = "マイナスの勝ち"
+                    } else {
+                        result = ""
+                    }
+                }
+                .font(.system(size: 40))
+                .foregroundStyle(.white)
+                .frame(width: 100, height: 100)
+                .background(.blue)
+            }
         }
-        .padding()
     }
 }
 
