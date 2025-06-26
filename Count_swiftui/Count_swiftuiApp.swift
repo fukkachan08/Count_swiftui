2c5cb0ff157b8559c65860f86b7a4e66c9f90c58
Fukano Masato <marbo0805@gmail.com>
2025-06-26 18:03:46 +0900

    Initial Commit

---
+++ Count_swiftui/Count_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Count_swiftuiApp.swift
+//  Count_swiftui
+//
+//  Created by 深野真人 on 2025/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Count_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
